b74be3ab68f39ca46fac09e18c12f4c52072893d
Igor Emanuel Carvalho da Cruz <igor.cruz1@estudante.ufla.br>
2021-04-21 23:38:58 -0300

    dicionario corrigido para switch case

--- app/src/views/City.vue
+++ app/src/views/City.vue
@@ -10,7 +10,8 @@
 
 <script>
 
-    
+    import { getLabelOption } from '../Utils/weatherHelper';
+    //import sp244 from '../Utils/previsao244.xml';
 
     export default {
         name: "Home",
@@ -27,7 +28,16 @@
             var url = new URL(url_string); 
             var pathname = url.pathname;
             this.id = pathname.split('/')[2];
+            
+            this.listar(this.id);
         },
+
+        methods: {
+
+            listar() {
+                console.log('lb', getLabelOption('ppct'));
+            }
+        }
 
     }
 </script>
